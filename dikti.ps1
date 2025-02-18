b7ef096526395570adbea6d1b15dbe8301d6bcc9
DNS <37093+DNS@users.noreply.github.com>
2025-02-18 14:58:26 +0700

    init

---
+++ dikti.ps1
@@ -0,0 +1,20 @@
+﻿
+$search = $args[0] -replace ' ', '+'
+#$search
+
+$url = 'https://api-pddikti.kemdiktisaintek.go.id/pencarian/mhs/'
+$ref = 'https://pddikti.kemdiktisaintek.go.id/'
+$ori = 'https://pddikti.kemdiktisaintek.go.id'
+
+$ip = $(irm https://api.ipify.org/?format=json).ip
+
+$a = iwr -Uri $url -Method Options -Headers @{'Referer'= $ref; 'Origin'=$ori }
+$b = irm -Uri "$url$search" -Method Get -ContentType 'application/json' -Headers @{'Referer'= $ref; 'X-User-IP'=$ip; 'Origin'=$ori }
+
+
+#$b = iwr -Uri "$url$search" -Method Get -ContentType 'application/json' -Headers @{'Referer'= $ref; 'X-User-IP'=$ip; 'Origin'=$ori }
+
+$b
+
+
+
