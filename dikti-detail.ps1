8d048cbe4908f02725e908ec1d95cbca3e1068a5
DNS <37093+DNS@users.noreply.github.com>
2025-02-18 15:58:10 +0700

    A: dikti-detail.ps1

---
+++ dikti-detail.ps1
@@ -0,0 +1,13 @@
+﻿
+$id = $args[0]
+
+$url = 'https://api-pddikti.kemdiktisaintek.go.id/detail/mhs/'
+$ref = 'https://pddikti.kemdiktisaintek.go.id/'
+$ori = 'https://pddikti.kemdiktisaintek.go.id'
+
+$ip = $(iwr https://api.ipify.org/).Content
+
+iwr -Uri "$url$id" -Method Options -Headers @{'Referer'= $ref; 'Origin'=$ori } | Out-Null
+irm -Uri "$url$id" -Method Get -ContentType 'application/json' -Headers @{'Referer'= $ref; 'X-User-IP'=$ip; 'Origin'=$ori }
+
+
