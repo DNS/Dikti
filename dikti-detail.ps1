f0e8e414e1933a4e584477b87bedeee9c708ec81
DNS <37093+DNS@users.noreply.github.com>
2025-02-19 13:16:26 +0700

    hyperlink

--- dikti-detail.ps1
+++ dikti-detail.ps1
@@ -10,4 +10,7 @@ $ip = $(iwr https://api.ipify.org/).Content
 iwr -Uri "$url$id" -Method Options -Headers @{'Referer'= $ref; 'Origin'=$ori } | Out-Null
 irm -Uri "$url$id" -Method Get -ContentType 'application/json' -Headers @{'Referer'= $ref; 'X-User-IP'=$ip; 'Origin'=$ori }
 
+"https://pddikti.kemdiktisaintek.go.id/detail-mahasiswa/$id"
+''
+
 
